9a8c397318229b1baec1ec1a8b8eb38bdc6cc3a9
kmspina <kmspina@wustl.edu>
2016-04-11 19:00:57 -0500

    all

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,6 @@
+alert("Help me fix this program");
+alert("Can you get this message to appear");
+document.write("<h1> Hello World! </h1>");
+document.write("<p> i wanna touch the earth, I wanna break it in my hand. I wanna grow something wild and unruly.  I wanna sleep on the hard group, .... With a pillow of blue bonnects and a blanket made of starts. It sounds good to me. Cowboy take me away, fly this girl as high as you can into the wild blue. Set me free, oh I pray. closer to heaven above and closer to you </p>");
+document.write("<p> I heard there was a secret chord that david played and it pleased the lord but you dont really care for music do you? well it goes like this the fourth the fifth the minor fall the major lift the baffled king composinging hallelujah </p>");
+document.write("<p> 3rd greenleaf song.  sorry i ran out of time and lyrics that we sing </p>");
---
+++ 04_vars/scripts.js
@@ -0,0 +1,12 @@
+var heading = "";
+var content= "";
+
+heading = "this heading";
+
+content += "<h1>" + heading + "</h1>"
+
+content += "<p> the first things</p>";
+
+content += "<p> the second things </p>";
+
+console.log(content);
